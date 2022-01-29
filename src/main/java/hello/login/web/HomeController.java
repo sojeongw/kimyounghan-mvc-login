47659b1225de698b526ace0426da479fb67540ef
sojeongw <sojeongw.official@gmail.com>
2022-01-30 06:44:37 +0900

    로그인 처리: 세션 적용하기

--- src/main/java/hello/login/web/HomeController.java
+++ src/main/java/hello/login/web/HomeController.java
@@ -2,6 +2,7 @@ package hello.login.web;
 
 import hello.login.domain.member.Member;
 import hello.login.domain.member.MemberRepository;
+import hello.login.web.session.SessionManager;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
@@ -9,35 +10,51 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.CookieValue;
 import org.springframework.web.bind.annotation.GetMapping;
 
+import javax.servlet.http.HttpServletRequest;
+
 @Slf4j
 @Controller
 @RequiredArgsConstructor
 public class HomeController {
 
     private final MemberRepository memberRepository;
+    private final SessionManager sessionManager;
 
     //    @GetMapping("/")
     public String home() {
         return "home";
     }
 
-    @GetMapping("/")
+    //    @GetMapping("/")
     /*
     HttpServletRequest 등 쿠키를 가져오는 방법은 많으나 스프링이 제공하는 @CookieValue를 사용해보자.
     로그인 안 한 사용자도 home에 접속할 수 있어야 하므로 required는 false로 둔다.
     들어오는 memberId는 String이지만 스프링이 Long으로 컨버팅해준다.
     */
     public String homeLogin(@CookieValue(name = "memberId", required = false) Long memberId, Model model) {
-        if(memberId == null) {
+        if (memberId == null) {
             return "home";
         }
 
         Member loginMember = memberRepository.findById(memberId);
-        if(loginMember == null) {
+        if (loginMember == null) {
             return "home";
         }
 
         model.addAttribute("member", loginMember);
+        return "loginHome";
+    }
+
+    @GetMapping("/")
+    public String homeLoginV2(HttpServletRequest request, Model model) {
+        // 세션 저장소에서 정보를 가져온다.
+        Member member = (Member) sessionManager.getSession(request);
+
+        if (member == null) {
+            return "home";
+        }
+
+        model.addAttribute("member", member);
         return "loginHome";
     }
 }
